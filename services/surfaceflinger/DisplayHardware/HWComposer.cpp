eb36c28a24fa58cec438e6149575908269193123
Franz-Josef Haider <f_haider@gmx.at>
2015-12-21 17:35:31 +0100

    send buffer info to sfdroid
    Change-Id: I4c5be78695f5fb0f62977b3052ed54663079f803

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -996,7 +996,7 @@ int HWComposer::fbPost(int32_t id,
         return setFramebufferTarget(id, acquireFence, buffer);
     } else {
         acquireFence->waitForever("HWComposer::fbPost");
-        return mFbDev->post(mFbDev, buffer->handle);
+        return mFbDev->post(mFbDev, buffer->handle, buffer->getWidth(), buffer->getHeight(), buffer->getStride(), buffer->getPixelFormat());
     }
 }
 
